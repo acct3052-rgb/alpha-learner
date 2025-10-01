92895cbe58fe09a9c30312cb9b72dd5223dee886
acct3052-rgb <acct3052@gmail.com>
2025-10-01 14:23:59 -0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
     url: 'https://fwrvvphfpurfxlknxrrj.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cnZ2cGhmcHVyZnhsa254cnJqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTMzNjU5MCwiZXhwIjoyMDc0OTEyNTkwfQ.b7VMCt8rg6twJ8wZ92s9eq8raoQDajh2xIl8TyWSuCY'
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cnZ2cGhmcHVyZnhsa254cnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMzY1OTAsImV4cCI6MjA3NDkxMjU5MH0.cDvxIbiToaVpZLhrWDZ7DC6H3lyCA7_B0udfeRigM1E'
 };
