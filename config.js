cee596f9bdd696b74b3b727b791c321657fa1561
acct3052-rgb <acct3052@gmail.com>
2025-10-01 14:16:13 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+    url: 'https://fwrvvphfpurfxlknxrrj.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cnZ2cGhmcHVyZnhsa254cnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMzY1OTAsImV4cCI6MjA3NDkxMjU5MH0.cDvxIbiToaVpZLhrWDZ7DC6H3lyCA7_B0udfeRigM1E'
+};
